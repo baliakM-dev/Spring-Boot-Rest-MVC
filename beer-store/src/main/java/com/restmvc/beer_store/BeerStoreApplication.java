6fcc316301bdb10dbc32c93cd8e5d92dc17f185a
baliakM-dev <baliak.m@hotmail.com>
2026-02-13 17:11:11 +0100

    Add project. Add dependency pom.xml. Create mysql profile and docker compose. Test running.

---
+++ beer-store/src/main/java/com/restmvc/beer_store/BeerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.restmvc.beer_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerStoreApplication.class, args);
+	}
+
+}
